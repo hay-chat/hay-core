b9dfbdbed1d99ca39e035213d5f2bfe48b58ece2
Roger Junior <roger@rogerjunior.com>
2026-02-06 13:17:32 +0000

    Add robots meta tag to improve SEO settings

--- dashboard/nuxt.config.ts
+++ dashboard/nuxt.config.ts
@@ -168,6 +168,7 @@ export default defineNuxtConfig({
         { charset: "utf-8" },
         { name: "viewport", content: "width=device-width, initial-scale=1" },
         { name: "description", content: "Hay platform dashboard application" },
+        { name: "robots", content: "noindex, nofollow" },
       ],
       link: [
         { rel: "preconnect", href: "https://fonts.googleapis.com" },
